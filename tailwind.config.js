5209e2d4565cf9869e03dffec04dd5420d0f468e
klsweetwood <klsweetwood@gmail.com>
2024-04-04 13:35:27 -0500

    add material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import withMT from '@material-tailwind/html/utils/withMT';
 
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: ['./src/**/*.{html,ts}'],
+  theme: {},
+  plugins: [],
+});
