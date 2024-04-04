5376c72a3c5c7f8c170ae55171f4a569de16e47a
klbrisson <klbrisson@gmail.com>
2024-04-04 09:49:47 -0500

    add transaction-history and add-transaction components

--- src/app/features/transactions/transactions-container.component.ts
+++ src/app/features/transactions/transactions-container.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AddTransactionComponent } from './components/add-transaction/add-transaction.component';
+import { TransactionHistoryComponent } from './components/transaction-history/transaction-history.component';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AddTransactionComponent, TransactionHistoryComponent],
   templateUrl: './transactions-container.component.html',
   styleUrl: './transactions-container.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
